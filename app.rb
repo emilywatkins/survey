d5f278976cf98fb8f2f208e3624cb034182aa889
Emily Watkins <emily.e.dolliver@gmail.com>
2018-02-27 14:24:08 -0800

    add crud functionality to surveys.

--- app.rb
+++ app.rb
@@ -17,3 +17,23 @@ post('/surveys') do
   @survey.save
   erb(:success)
 end
+
+get('/surveys/:id/edit') do
+  @survey = Survey.find(params['id'].to_i)
+  erb(:survey_edit)
+end
+
+patch('/surveys/:id') do
+  title = params['title']
+  @survey = Survey.find(params['id'].to_i)
+  @survey.update({:title => title})
+  @surveys = Survey.all
+  erb(:index)
+end
+
+delete('/surveys/:id') do
+  @survey = Survey.find(params['id'].to_i)
+  @survey.destroy
+  @surveys = Survey.all
+  erb(:index)
+end
